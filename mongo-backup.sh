79f93aeffd44e0c761c783375d688cc5cfeaf104
dubblebee <brianbenton77@gmail.com>
2017-12-08 13:48:42 +0000

    Replace time delimiters to '.'

--- mongo-backup.sh
+++ mongo-backup.sh
@@ -2,7 +2,9 @@
 
 # Assign variables & set-up files
 WKDIR=/data/backup
-DIR=`date +%F-%T`
+DIR2=`date +%F-%T`
+# Replace the ':' with '.' in the date string (causes problems when archiving)
+DIR=$(echo $DIR2 | tr ':' '.')
 DEST=/data/backup/$DIR
 ERR='zz_FAILED_BACKUP_'$DIR
 LOG=/data/backup_logs/backup.log
@@ -17,7 +19,8 @@ cp /var/scripts/backup/email/failure-template.html /var/scripts/backup/email/fai
 # Create the backup folder
 mkdir $DEST
 # Run mongodump - backup all databases
-/usr/bin/mongodump --host localhost --port 27017 --username=admin --password=anORgdGl67mWJAowR6VqIfzdQi52AlbB --authenticationDatabase=admin --out $DEST \
+#/usr/bin/mongodump --host localhost --port 27017 --username=admin --password=anORgdGl67mWJAowR6VqIfzdQi52AlbB --authenticationDatabase=admin --out $DEST \
+/usr/bin/mongodump --host localhost --port 27017 --username=mongo --password=mongo --authenticationDatabase=admin --out $DEST \
 >> $LOG 2>&1
 
 
@@ -32,10 +35,10 @@ if [ $? -ne 0 ]; then
     # Log the events
     echo "*** MongoDump failed! ***" | tee -a "$LOG"
 
-    # Send email notification of failure
+    # Construct email notification of failure
     sed -i -e "s~%%FAILURE%%~${failmsg}~g" /var/scripts/backup/email/failure-notification.html
     
-    # Do NOT indent Email headers & EOF below
+    # Send email - !! DO NOT indent Email headers & EOF below !!
     cat <<'EOF' - /var/scripts/backup/email/failure-notification.html | /usr/sbin/sendmail -i -t
 To: brian.benton@bbc.co.uk
 Subject: MongoDB Backup failed
@@ -45,26 +48,22 @@ EOF
     
     exit 1
 else
-	# Log the events
-	echo "*** Backup complete - Added $DEST ***"  | tee -a "$LOG"
+    # Log the events
+    echo "*** Backup complete - Added $DEST.bz2  ***"  | tee -a "$LOG"
 
     # Remove the oldest backup folder
-    min_dirs=3
-	[[ $(find "$WKDIR" -maxdepth 1 -type d | wc -l) -ge $min_dirs ]] &&
-	IFS= read -r -d $'\0' line < <(find "$WKDIR" -maxdepth 1 -printf '%T@ %p\0' 2>/dev/null | sort -z -n)
-	file="${line#* }"
-	echo "REMOVED " $file &>> $REMLOG
+    min_dirs=0
+    [[ $(find "$WKDIR" -maxdepth 1 -type d | wc -l) -ge $min_dirs ]] &&
+    IFS= read -r -d $'\0' line < <(find "$WKDIR" -maxdepth 1 -printf '%T@ %p\0' 2>/dev/null | sort -z -n)
+    file="${line#* }"
+    echo "REMOVED " $file &>> $REMLOG
     remdir=$file
-	rm -rf "$file"
+    rm -rf "$file"
     
+    # Construct email notification of success
+    sed -i -e "s~%%ADDED%%~${DEST}.bz2~g" -e "s~%%REMOVED%%~${remdir}~g" /var/scripts/backup/email/success-notification.html
     
-    # Send email notification of success
-    sed -i -e "s~%%ADDED%%~${DIR}~g" -e "s~%%REMOVED%%~${remdir}~g" /var/scripts/backup/email/success-notification.html
-
-    # echo $NEWDIR
-    # echo $REMDIR
-    
-    # Do NOT indent Email headers & EOF below
+    # Send email - !! DO NOT indent Email headers & EOF below !!
     cat <<'EOF' - /var/scripts/backup/email/success-notification.html | /usr/sbin/sendmail -i -t
 To: brian.benton@bbc.co.uk
 Subject: MongoDB Backup completed
@@ -74,5 +73,11 @@ EOF
 
 fi
 
-# Make an archive copy of the backup.log file
-cp /data/backup_logs/backup.log /data/backup_logs/${DIR}_backup.log
+# Archive and compress (bzip) the backup
+tar -cjf ${DEST}.bz2 ${DEST}
+
+# Remove the raw backup files
+rm -R ${DEST}
+
+# Make an archive copy of the backup.log file - <-- NOT NEEDED, NOW USES logrotate (/etc/logrotate.conf)
+# cp /data/backup_logs/backup.log /data/backup_logs/${DIR}_backup.log
